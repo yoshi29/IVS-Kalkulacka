89a838670757f1ac833896083881f8df16bd07df
yoshi29 <yoshi682000@gmail.com>
2020-03-09 18:40:57 +0100

    Basic structure of project

---
+++ src/Calculator/MathLibrary/MathLib.cs
@@ -0,0 +1,43 @@
+
+using System;
+
+namespace MathLibrary
+{
+    public class MathLib
+    {
+        public double Add(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Sub(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Mul(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Div(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Fact(double num)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Pow(double number, double power)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Sqrt(double number, double degree)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
